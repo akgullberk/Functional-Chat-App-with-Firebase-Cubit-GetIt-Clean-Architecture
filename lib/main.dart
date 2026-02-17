97105f7830866ccefe266b1d7c65ef458961fa54
Berk Akgül <108866525+akgullberk@users.noreply.github.com>
2026-02-17 12:00:07 +0300

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:messenger_app/config/theme/app_theme.dart';
+import 'package:messenger_app/firebase_options.dart';
 import 'package:messenger_app/presentation/screens/auth/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -20,4 +24,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
